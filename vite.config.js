c62708012b5bff6ef626a4412bd8f0cb729d9575
Ben Cooley <bcooley@uvm.edu>
2024-11-12 16:01:38 -0500

    test build to docs dir

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   optimizeDeps: {
     include: ['mapbox-gl', 'flatgeobuf']
   },
   resolve: {
     alias: {
-      'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js'
+      // Use the explicit path without dist/
+      'mapbox-gl': 'mapbox-gl',
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   plugins: [
     vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  ]
 })
