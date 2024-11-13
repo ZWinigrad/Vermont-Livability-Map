6e92d0df52c1cba5fa17fdd33dd895841a5795bc
Kendall <25021027+kefortney@users.noreply.github.com>
2024-11-13 09:40:36 -0500

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,9 +10,18 @@
         Key partners who provided additional staffing to organize and lead sub-teams included Kendall Fortney of the <a href="https://verso.w3.uvm.edu/" target="_blank">Vermont Open Source Research Office (VERSO)</a> at the <a href="https://uvm.edu/" target="_blank">University of Vermont</a>, Catherine Dimitruk of the <a href="https://www.nrpcvt.com/" target="_blank">Northwest Regional Planning Commission</a>, and Leslie Black-Plumeau of the <a href="https://vhfa.org/" target="_blank">Vermont Housing Finance Agency</a>. Significant funding for student interns was provided by the <a href="https://www.uvm.edu/cas/geography" target="_blank">University of Vermont Geography and Geosciences Department</a>, <a href="https://www.uvm.edu/gund" target="_blank">Gund Fund for the Environment</a>, the College of Arts & Sciences Fellowship program and an anonymous donation which supported the UVM ORCA Student internship teams for over a year.
         </p>
         <p>
-       In August 2024, the project officially transferred ownership to VERSO and the University of Vermont’s Open Source Programs Office under the leadership of Director Kendall Fortney, which will continue to coordinate the ongoing collection of a custom data subset in partnership with the Regional Planning Commissions, Town Planners, the Agency of Commerce and Community Development, and the Vermont Center for Geographic Information. Additional projects, like this site, will be created to explore the Vermont Zoning Atlas data and its implications for Vermont’s future along with other datasets.
+        The project was also presented at the <a href="https://www.uvm.edu/uvminnovations/invention-2-venture-i2v-conference" target="_blank">2024 Invention2Venture</a>  April 18, 2024 at the University of Vermont as part of a talk on the <a href="https://verso.w3.uvm.edu/orca/" target="_blank">Open Research Community Accelerator (ORCA)</a> program with Zoe Srenden presenting along with other team members.
+        </p><br>
+        <img alt="Rise Summit" src="@/assets/rise_summit.png"/>
+        <p><br>
+        <p>
+        The Open Research Community Accelerator (ORCA) is a student program that grows the impact of academic research by making research products accessible to other academics, local businesses, and community members through open source project. ORCA creates these products through student project teams called Pods — groups of dynamic and passionate students who learn industry open source practices by working on real projects destined for public use. This program is run and funded by the University of Vermont Open Source Program Office (VERSO). ORCA’s goals are to produce impactful, accessible, open source research tools by training tomorrow’s workforce in marketable open source skills.
+        </p><br>
+        <img alt="ORCA at Work" src="@/assets/classwork.png"/>
+        <p><br>
+        In August 2024, the project officially transferred ownership to VERSO and the University of Vermont’s Open Source Programs Office under the leadership of Director Kendall Fortney, which will continue to coordinate the ongoing collection of a custom data subset in partnership with the Regional Planning Commissions, Town Planners, the Agency of Commerce and Community Development, and the Vermont Center for Geographic Information. Additional projects, like this site, will be created to explore the Vermont Zoning Atlas data and its implications for Vermont’s future along with other datasets.
         </p>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="45" height="45" />
+      
     </div>
   </div>
 </template>
